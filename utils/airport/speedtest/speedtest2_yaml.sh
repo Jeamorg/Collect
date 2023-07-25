2c17a232d6e06ebdde60e2edf7fe22bf0cd11ae1
Jeamorg <93057377+Jeamorg@users.noreply.github.com>
2023-07-25 11:40:37 +0800

    Add files via upload

---
+++ utils/airport/speedtest/speedtest2_yaml.sh
@@ -0,0 +1,7 @@
+#准备好所需文件
+wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v0.15.0.gz
+gzip -d lite-linux-amd64.gz
+wget -O lite_config.json https://c.jeam.cc/utils/airport/speedtest/lite_config_yaml_airport.json
+#运行 LiteSpeedTest
+chmod +x ./lite-linux-amd64
+sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://c.jeam.cc/sub/airport_merge_yaml.yml > speedtest.log 2>&1 &
